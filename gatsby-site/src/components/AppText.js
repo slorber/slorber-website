dd12961b7df5bdab467cb2dea35da7b9ef566224
slorber <lorber.sebastien@gmail.com>
2018-03-20 10:50:45 +0100

    add getFontSizeStyle

--- gatsby-site/src/components/AppText.js
+++ gatsby-site/src/components/AppText.js
@@ -6,13 +6,48 @@ const styles = StyleSheet.create({
   defaultText: {
     fontFamily: "roboto",
   },
+  xl: {
+    fontSize: 32,
+  },
+  l: {
+    fontSize: 26,
+  },
+  m: {
+    fontSize: 22,
+  },
+  s: {
+    fontSize: 16,
+  },
 });
+
+
+const getFontSizeStyle = (props) => {
+  if ( props.fontSize ) {
+    return {fontSize: props.fontSize};
+  }
+  else if ( props.xl ) {
+    return styles.xl;
+  }
+  else if ( props.l ) {
+    return styles.l;
+  }
+  else if ( props.m ) {
+    return styles.m;
+  }
+  else if ( props.s ) {
+    return styles.s;
+  }
+  else {
+    return styles.m;
+  }
+};
 
 const AppText = props => (
   <RNText
     {...props}
     style={[
       styles.defaultText,
+      getFontSizeStyle(props),
       props.style,
     ]}
   />
